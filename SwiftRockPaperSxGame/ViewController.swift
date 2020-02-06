af31cd6a2ff3477239c50506e054e00f3869d288
Brian <rskace@gmail.com>
2020-02-06 15:19:26 -0800

    choice select done

--- SwiftRockPaperSxGame/ViewController.swift
+++ SwiftRockPaperSxGame/ViewController.swift
@@ -8,15 +8,51 @@
 
 import UIKit
 
-
-
+var score: Int = 0
+var cpuChoice: String = ""
+var playerChoice: String = ""
 
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var scoreView: UILabel!
+    @IBOutlet weak var winLoseView: UILabel!
+
+    @IBOutlet weak var cpuView: UIImageView!
+    
+    @IBOutlet weak var resetButton: UIButton!
+    
+    let imageNames = ["rock", "scissors", "paper"]
+    
+    @IBAction func rockButton(_ sender: UIButton) {
+        playerChoice = "rock"
+        winLoseView.text = playerChoice
+    }
+    
+    
+    @IBAction func paperButton(_ sender: UIButton) {
+        playerChoice = "paper"
+        winLoseView.text = playerChoice
+    }
+    
+    @IBAction func scisscorsButton(_ sender: UIButton) {
+        playerChoice = "scissors"
+        winLoseView.text = playerChoice
+    }
+    
+    
+    
+    
+    
+    
     
     
 
+    
+    @IBAction func resetButton(_ sender: UIButton) {
+        score = 0
+        scoreView.text = "Score " + String(score)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
